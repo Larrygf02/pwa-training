ed1a996829172b17433fc9fd20e77d9466e37bd9
Larrygf02 <raulgf_02@hotmail.com>
2020-06-04 21:39:35 -0500

    Activate service worker

--- sw.js
+++ sw.js
@@ -2,3 +2,8 @@
 self.addEventListener('install', evt => {
     console.log('Service worker has been installed')
 });
+
+// activate event service worker
+self.addEventListener('activate', evt => {
+    console.log('service worker has been activated')
+})
